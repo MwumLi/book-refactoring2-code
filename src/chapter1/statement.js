171ae2e715ff9dfa61f3e2417f51a4d7375738b0
MwumLi <mwumli@hotmail.com>
2021-01-20 23:37:37 +0800

    refactor(1.4): mv perf to aPerformance

--- src/chapter1/statement.js
+++ src/chapter1/statement.js
@@ -23,21 +23,21 @@ function statement(invoice, plays) {
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
 
-  function amountFor(perf, play) {
+  function amountFor(aPerformance, play) {
     let result = 0;
     switch (play.type) {
       case "tragedy":
         result = 40000;
-        if (perf.audience > 30) {
-          result += 1000 * (perf.audience - 30);
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
         }
         break;
       case "comedy":
         result = 30000;
-        if (perf.audience > 20) {
-          result += 10000 + 500 * (perf.audience - 20);
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
         }
-        result += 300 * perf.audience;
+        result += 300 * aPerformance.audience;
         break;
       default:
         throw new Error(`unknown type: ${play.type}`);
