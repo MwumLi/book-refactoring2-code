5a96680357382fe7a2589c57f43db9751b8aff90
MwumLi <mwumli@hotmail.com>
2021-02-15 18:30:43 +0800

    refactor(1.4): 采用内联变量手法内联 totalVolumeCredits

--- src/chapter1/statement.js
+++ src/chapter1/statement.js
@@ -8,10 +8,8 @@ function statement(invoice, plays) {
     totalAmount += amountFor(perf);
   }
 
-  let volumeCredits = totalVolumeCredits();
-
   result += `Amount owed is ${usd(totalAmount / 100)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
 
   function totalVolumeCredits() {
     let volumeCredits = 0;
