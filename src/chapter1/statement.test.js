53e6f933dfa40f8670abf653243ac60aa39ef359
MwumLi <mwumli@hotmail.com>
2021-02-15 22:18:21 +0800

    feat(1.7): 增加 htmlStatement 去支持 html 输出

--- src/chapter1/statement.test.js
+++ src/chapter1/statement.test.js
@@ -1,4 +1,4 @@
-import { statement } from './statement';
+import { statement, htmlStatement } from './statement';
 import { expect } from 'chai';
 describe('chapter1', function () {
   it('statement(invoice, plays)', function () {
@@ -11,6 +11,21 @@ describe('chapter1', function () {
       + 'Amount owed is $1,730.00\n'
       + 'You earned 47 credits\n';
     expect(statement(invoices[0], plays)).to.be.equal(expectResult);
+  });
+
+  it('htmlStatement(invoice, plays)', function () {
+    const invoices = require('./invoices.json')
+    const plays = require('./plays.json')
+    const expectResult = '<h1>Statement for BigCo</h1>\n'
+      + '<table>\n'
+      + '<tr><th>play</th><th>seats</th><th>cost</th></tr>'
+      + ' <tr><td>Hamlet</td><td>55</td><td>$65,000.00</td></tr>\n'
+      + ' <tr><td>As You Like It</td><td>35</td><td>$58,000.00</td></tr>\n'
+      + ' <tr><td>Othello</td><td>40</td><td>$50,000.00</td></tr>\n'
+      + '</table>\n'
+      + '<p>Amount owed is <em>$173,000.00</em></p>\n'
+      + '<p>You earned <em>47</em> credits</p>\n';
+    expect(htmlStatement(invoices[0], plays)).to.be.equal(expectResult);
   });
 });
 
