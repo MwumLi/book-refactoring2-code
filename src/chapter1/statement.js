b10ed6fe7a94f3be826b61ecc6a23e8e7f7e2216
MwumLi <mwumli@hotmail.com>
2021-02-15 21:41:53 +0800

    refactor(1.6): 用同样的手法移动观众量积分的计算 volumeCreditsFor

--- src/chapter1/statement.js
+++ src/chapter1/statement.js
@@ -19,7 +19,7 @@ function renderPlainText(data) {
   function totalVolumeCredits() {
     let volumeCredits = 0;
     for (let perf of data.performances) {
-      volumeCredits += volumeCreditsFor(perf);
+      volumeCredits += perf.volumeCredits;
     }
     return volumeCredits;
   }
@@ -30,14 +30,6 @@ function renderPlainText(data) {
       currency: "USD",
       minimumFractionDigits: 2,
     }).format(aNumber);
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-    return result;
   }
 }
 function statement(invoice, plays) {
@@ -50,6 +42,7 @@ function statement(invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
@@ -76,6 +69,14 @@ function statement(invoice, plays) {
       default:
         throw new Error(`unknown type: ${aPerformance.play.type}`);
     }
+    return result;
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === aPerformance.play.type)
+      result += Math.floor(aPerformance.audience / 5);
     return result;
   }
 }
