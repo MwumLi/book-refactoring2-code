9ebe6e0b90cad24b4ac2abd635a02dfc2f17f61e
MwumLi <mwumli@hotmail.com>
2021-02-15 21:17:18 +0800

    refactor(1.6): 创建中转数据结构 statementData

--- src/chapter1/statement.js
+++ src/chapter1/statement.js
@@ -1,4 +1,4 @@
-function renderPlainText(invoice, plays) {
+function renderPlainText(data, invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
@@ -67,7 +67,8 @@ function renderPlainText(invoice, plays) {
   }
 }
 function statement(invoice, plays) {
-  return renderPlainText(invoice, plays);
+  const statementData = {};
+  return renderPlainText(statementData, invoice, plays);
 }
 
 module.exports = statement
