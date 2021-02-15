3d797c4bfbbcecea319b6ac3f6782558867b25b5
MwumLi <mwumli@hotmail.com>
2021-02-15 21:26:38 +0800

    refactor(1.6): 增加 enrichPerformance 函数返回 performanace 的副本, 尽量保持数据不可变

--- src/chapter1/statement.js
+++ src/chapter1/statement.js
@@ -69,8 +69,13 @@ function renderPlainText(data, plays) {
 function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances;
+  statementData.performances = invoice.performances.map(enrichPerformance);
   return renderPlainText(statementData, plays);
+
+  function enrichPerformance(aPerformance) {
+    const result = Object.assign({}, aPerformance);
+    return result;
+  }
 }
 
 module.exports = statement
