09ad1518b9a2090ff388b44c17b6fc0ede073f4e
MwumLi <mwumli@hotmail.com>
2021-02-15 22:39:28 +0800

    refactor(1.8): 准备好多态类结构 TragedyCalculator/ComedyCalculator

--- src/chapter1/createStatementData.js
+++ src/chapter1/createStatementData.js
@@ -35,8 +35,17 @@ class PerformanceCalculator {
   }
 }
 
+class TragedyCalculator extends PerformanceCalculator { }
+class ComedyCalculator extends PerformanceCalculator { }
 function createPerformanceCalculator(aPerformance, aPlay) {
-  return new PerformanceCalculator(aPerformance, aPlay);
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`unknown type: ${aPlay.type}`);
+  }
 }
 
 export default function createStatementData(invoice, plays) {
