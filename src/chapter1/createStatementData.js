51155cfbf528e0a400a297a64c77c716c6a89007
MwumLi <mwumli@hotmail.com>
2021-02-15 22:36:12 +0800

    refactor(1.8) 移动观众量积分的计算到 PerformanceCalculator

--- src/chapter1/createStatementData.js
+++ src/chapter1/createStatementData.js
@@ -25,6 +25,14 @@ class PerformanceCalculator {
     }
     return result;
   }
+
+  get volumeCredits() {
+    let result = 0;
+    result += Math.max(this.performance.audience - 30, 0);
+    if ("comedy" === this.play.type)
+      result += Math.floor(this.performance.audience / 5);
+    return result;
+  }
 }
 
 export default function createStatementData(invoice, plays) {
@@ -43,20 +51,12 @@ export default function createStatementData(invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
     result.amount = calculator.amount;
-    result.volumeCredits = volumeCreditsFor(result);
+    result.volumeCredits = calculator.volumeCredits;
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-    return result;
   }
 
   function totalAmount(data) {
